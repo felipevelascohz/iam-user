abf1590886da84a9a7c3d05c4994fad1183d06d4
Felipe Velasco <fvelasc1@bancoestado.cl>
2023-08-08 18:36:48 -0400

    first commit

--- iam-user.d.ts
+++
@@ -1,5 +0,0 @@
-import { Construct } from 'constructs';
-import * as cdk from 'aws-cdk-lib';
-export declare class IamUser extends cdk.Stack {
-    constructor(stack: Construct, id: string, props?: cdk.StackProps);
-}
